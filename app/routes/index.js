84f10663130e3b14c1e5e2d6218367f8d0ae68b2
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-26 19:49:11 -0700

    correct a bug

--- app/routes/index.js
+++ app/routes/index.js
@@ -38,7 +38,7 @@ module.exports = function (app, passport) {
 
 	app.route('/api/:id')
 		.get(isLoggedIn, function (req, res) {
-			console.log("TWITERRRRRRRRRRR"+req.user.twitter);
+			console.log("GITHUBBB"+req.user.github);
 			if(req.user.github !== null)
 				res.json(req.user.github);
 			else if(req.user.twitter != null)
