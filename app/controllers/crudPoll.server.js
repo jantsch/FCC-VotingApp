4ef7c63f99bc36e37a018362c6c73fff097b0a0f
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-28 13:49:01 -0700

    New Poll

--- app/controllers/crudPoll.server.js
+++ app/controllers/crudPoll.server.js
@@ -18,8 +18,8 @@ function PollHandler () {
 	this.getMyPolls = function (req, res) {
 
 		console.log("Server");
-		console.log(req.user.getObj()._id);
-		Poll.find({'owner_id': req.user.getObj()._id}).exec(function(err,result){
+		console.log(req.user._id);
+		Poll.find({'owner_id': req.user._id}).exec(function(err,result){
 				if (err) { throw err; }				
 				res.json(result);
 
