26c2ef265e032c1f0a6574397c3ff173897dfa5a
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-04-28 12:53:26 -0700

    New Poll

--- app/controllers/pollController.client.js
+++ app/controllers/pollController.client.js
@@ -12,10 +12,10 @@
 
    function updatePoll (data) {
       console.log("Client");
-      console.log(JSON.parse(data));
+      //console.log(JSON.parse(data));
       
       var data_parsed = JSON.parse(data);
-      console.log(JSON.parse(data).nome);
+      console.log(data_parsed.nome);
       question_name.text(data_parsed.name);
       question_owner.text("by " + data_parsed.question_owner);
    }
