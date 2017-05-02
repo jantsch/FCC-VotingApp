364858def27a1d937ca32d3451dafcbfde56e946
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-05-01 20:54:01 -0700

    Graph

--- app/controllers/pollController.client.js
+++ app/controllers/pollController.client.js
@@ -36,8 +36,8 @@ $(document).ready(function(){
                           startAngle: 60,                          
                           toolTipContent: "{legendText}: {y} - <strong>#percent% </strong>",          
                           showInLegend: true,
-                          dataPoints: [datapoints.toString()    
-                          ]
+                          dataPoints: datapoints.toString()    
+                          
                         }
                     ]
                   });
