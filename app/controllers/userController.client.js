c4cdcd78c4812c8612e0344404c8ec3aa641b78e
Rodrigo Jantsch <rodrigo_jantsch@hotmail.com>
2017-05-02 11:32:40 -0700

    Public

--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -4,7 +4,6 @@
 
    var profileId = document.querySelector('#profile-id') || null;
    var profileUsername = document.querySelector('#profile-username') || null;
-   var profileRepos = document.querySelector('#profile-repos') || null;
    var displayName = document.querySelector('#display-name');
    var apiUrl = appUrl + '/api/user';
 
@@ -14,6 +13,7 @@
 
    ajaxFunctions.ready(ajaxFunctions.ajaxRequest('GET', apiUrl, function (data) {
       var userObject = JSON.parse(data);
+      console.log(userObject);
 
       if (userObject.displayName !== null) {
          updateHtmlElement(userObject, displayName, 'displayName');
